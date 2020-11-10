11b71e9d01274a36448f6ca46798a81387636b19
Jonathan Rockway <2367+jrockway@users.noreply.github.com>
2020-11-10 10:02:40 -0500

    print some version info

--- cmd/goodevening/main.go
+++ cmd/goodevening/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+var (
+	version string
+	commit  string
+	date    string
+)
 
 func main() {
+	if len(os.Args) == 2 && os.Args[1] == "version" {
+		fmt.Printf("Version %s\nBuild from %s on %s\n", version, commit, date)
+	}
 	fmt.Println("Good evening, Universe!")
 }
